876aea2737b5f197ae5626e2816543a1f09cacda
uael <lucas.abel@yandex.com>
2018-09-02 11:23:46 +0200

    fixed omz key bindings not working

--- zsh/.zplug.zsh
+++ zsh/.zplug.zsh
@@ -7,7 +7,7 @@ zplug 'zplug/zplug', hook-build:'zplug --self-manage'
 
 # From oh-my-zsh lib
 zplug 'lib/completion',   from:oh-my-zsh
-zplug 'lib/key-bindings', from:oh-my-zsh
+zplug 'lib/key-bindings', from:oh-my-zsh, defer:3
 
 # From oh-my-zsh plugins
 zplug 'plugins/adb',        from:oh-my-zsh
@@ -26,7 +26,6 @@ zplug 'modules/history',     from:prezto
 zplug 'modules/directory',   from:prezto
 zplug 'modules/spectrum',    from:prezto
 zplug 'modules/utility',     from:prezto
-zplug 'modules/emacs',       from:prezto
 zplug 'modules/fasd',        from:prezto
 zplug 'modules/git',         from:prezto
 
@@ -35,6 +34,6 @@ zstyle ':prezto:module:editor' dot-expansion 'yes'
 
 # From zsh-users
 zplug "zsh-users/zsh-completions",              defer:0
-zplug "zsh-users/zsh-autosuggestions",          defer:2, on:"zsh-users/zsh-completions"
-zplug "zsh-users/zsh-syntax-highlighting",      defer:2, on:"zsh-users/zsh-autosuggestions"
-zplug "zsh-users/zsh-history-substring-search", defer:3, on:"zsh-users/zsh-syntax-highlighting"
+zplug "zsh-users/zsh-autosuggestions",          defer:1, on:"zsh-users/zsh-completions"
+zplug "zsh-users/zsh-syntax-highlighting",      defer:1, on:"zsh-users/zsh-autosuggestions"
+zplug "zsh-users/zsh-history-substring-search", defer:2, on:"zsh-users/zsh-syntax-highlighting"
