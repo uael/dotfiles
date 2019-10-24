7971fa9baeb985c7142e4b62f34e5686bd8ee6f0
uael <lucas.abel@yandex.com>
2019-10-24 21:05:04 +0200

    added nixos home-manager

---
+++ .config/nixpkgs/home.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    alacritty
+    alsaUtils
+    arandr
+    gcc-arm-embedded
+    brave
+    ccls
+    jetbrains.clion
+    cmake
+    dmenu
+    gawk
+    gcc
+    gdb
+    git
+    gnumake
+    gnupg
+    gnuplot
+    hexedit
+    pkgs.lldb
+    nerdfonts
+    okular
+    pango
+    pavucontrol
+    python
+    qbittorrent
+    qemu
+    slock
+    tree
+    valgrind
+    vlc
+    vscode
+    wireshark
+  ];
+
+  fonts.fontconfig.enableProfileFonts = true;
+
+  gtk = {
+    enable = true;
+    font.name = "FuraCode Nerd Font";
+    theme.package = pkgs.adapta-gtk-theme;
+    theme.name = "adapta";
+  };
+}
